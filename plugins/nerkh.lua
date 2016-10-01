f45f1a6495f42c510bb124d8a0d1ee9e6f065c61
allwenandashi <ashkanpj.ap55@gmail.com>
2016-10-01 20:33:18 +0330

    Update nerkh.lua

--- plugins/nerkh.lua
+++ plugins/nerkh.lua
@@ -2,7 +2,7 @@
  function run(msg, matches)
  local reply_id = msg['id']
  local text = 'nerkh'
- local text1 ="  قيمت گروه هاي آنتي اسپم :\n\n\n  ماهيانه سوپرگروه 5000 تومان\n  دو ماهه سوپرگروه9000 تومان\n   سه ماهه سوپرگروه 14000 تومان\n\n —-----------------------------\n\n سفارش : @Efy_GOD_OF_HUNTER"
+ local text1 ="  قيمت گروه هاي آنتي اسپم :\n\n\n  ماهيانه سوپرگروه 5000 تومان\n  دو ماهه سوپرگروه9000 تومان\n   سه ماهه سوپرگروه 14000 تومان\n\n —-----------------------------\n\n سفارش : @MY_WIFE_IS_MEDIYA "
    reply_msg(reply_id, text1, ok_cb, false)
  end
  return {
